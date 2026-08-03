49ad5d1a8047de287bcb219b5c2568620970ebb4
saiduhassannuhu94-lang <saiduhassannuhu94@gmail.com>
2026-08-03 23:57:42 +0100

    Add Intro and MainMenu components to App

--- src/src/src/src/components/src/src/components/src/App.tsx
+++ src/src/src/src/components/src/src/components/src/App.tsx
@@ -1,1 +1,14 @@
+import { useState } from "react";
+import "./styles.css";
+import MainMenu from "./components/MainMenu";
+import Intro from "./components/Intro";
 
+export default function App() {
+  const [showIntro, setShowIntro] = useState(true);
+
+  if (showIntro) {
+    return <Intro onFinish={() => setShowIntro(false)} />;
+  }
+
+  return <MainMenu />;
+}
