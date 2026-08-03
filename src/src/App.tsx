b64c30bf9eba5410238f19be8962b18e82cc6a3e
saiduhassannuhu94-lang <saiduhassannuhu94@gmail.com>
2026-08-03 23:50:57 +0100

    Add Home component with routing in App.tsx

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,1 +1,49 @@
+import { Routes, Route } from "react-router-dom";
 
+function Home() {
+  return (
+    <div
+      style={{
+        background: "#000",
+        color: "#fff",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        fontFamily: "sans-serif"
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "60px",
+          marginBottom: "10px"
+        }}
+      >
+        NEXORA
+      </h1>
+
+      <p>The Last Awakening</p>
+
+      <button
+        style={{
+          marginTop: "30px",
+          padding: "15px 35px",
+          fontSize: "20px",
+          cursor: "pointer"
+        }}
+      >
+        PLAY
+      </button>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+          }
