d536f799c5ba56d11373615ed2385157cf53d2fb
saiduhassannuhu94-lang <saiduhassannuhu94@gmail.com>
2026-08-03 23:53:15 +0100

    Create MainMenu component with styled layout

--- src/src/src/src/components/MainMenu.tsx
+++ src/src/src/src/components/MainMenu.tsx
@@ -1,1 +1,57 @@
+export default function MainMenu() {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        inset: 0,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background:
+          "linear-gradient(to bottom,#030712,#081b29,#0f172a)"
+      }}
+    >
+      <h1
+        style={{
+          color: "#00ffff",
+          fontSize: "70px",
+          letterSpacing: "8px"
+        }}
+      >
+        NEXORA
+      </h1>
 
+      <p
+        style={{
+          color: "white",
+          marginTop: 15
+        }}
+      >
+        THE LAST AWAKENING
+      </p>
+
+      <button
+        style={{
+          marginTop: 40,
+          width: 220,
+          height: 60,
+          fontSize: 22
+        }}
+      >
+        ▶ PLAY
+      </button>
+
+      <button
+        style={{
+          marginTop: 15,
+          width: 220,
+          height: 60,
+          fontSize: 20
+        }}
+      >
+        ⚙ SETTINGS
+      </button>
+    </div>
+  );
+    }
